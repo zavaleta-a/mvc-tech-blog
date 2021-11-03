0353aae354e1879f348834095471ab56dfee7fba
Alfredo Zavaleta <zavaleta@Alfredos-MacBook-Pro.local>
2021-11-03 01:20:20 -0500

    creating userRoutes to get and create users

--- controllers/api/projectRoutes.js
+++
@@ -1,3 +0,0 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const 
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,88 +1,58 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-
-// route to get all 
-router.get('/', async (req, res) => {
-    const User = await User.findAll().catch((err) => { 
-      res.json(err);
-    });
-    // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it. 
-    const dishes = dishData.map((dish) => dish.get({ plain: true }));
-    // We render the template, 'all', passing in dishes, a new array of serialized objects.
-    res.render('all', { dishes });
-    });
-
-// route to get one dish
-router.get('/dish/:id', async (req, res) => {
-  try{ 
-      const dishData = await Dish.findByPk(req.params.id);
-      if(!dishData) {
-          res.status(404).json({message: 'No dish with this id!'});
-          return;
-      }
-      const dish = dishData.get({ plain: true });
-      res.render('dish', dish);
-    } catch (err) {
+//Get users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: {
+            exclude: ['password']
+        }
+    }).then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
         res.status(500).json(err);
-    };     
+    });
 });
 
-module.exports = router;
+//Get user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id
+        },
+        include: [{
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+                model: Post,
+                attributes: ['title']
+            }
+        }]
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({
+                message: 'User not found'
+            });
+            return;
+        } res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
+//Create user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+})
