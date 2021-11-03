6e4533e73c8321bbd4f0d03dfcda70aa6027d5a5
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-11-03 16:00:57 -0500

    updating index.js in the models folder

--- models/index.js
+++ models/index.js
@@ -1,17 +1,29 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
-class Comments extends Model {}
+const User = require("./user");
+const Posts = require("./posts");
+const Comments = require("./comments");
 
-Comments.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  comment_text: {
-      type: DataTypes.STRING,
-      validate: {
-          
-      }
-  }
+User.hasMany(Posts, {
+  foreignKey: "user_id",
 });
+
+Posts.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comments, {
+  foreignKey: "user_id",
+});
+
+Comments.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Posts.hasMany(Comments, {
+  foreignKey: "post_id",
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: "posts_id",
+});
+
+module.exports = { User, Posts, Comments };
